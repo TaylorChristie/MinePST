1d0563819a60de22eae8a29c55b56e47f877d708
Taylor <MineSQL@users.noreply.github.com>
2016-07-07 16:13:43 -0600

    Create DatabaseInterface.php

---
+++ vendor/MineSQL/DatabaseInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+// This is a loose interface for our database which we can hook into a MYSQL 
+// implementation, and then eventually a high level Paste interaction class
+// these interactions should have a table defined.
+
+abstract class DatabaseInterface {
+
+    public $connection, $table;
+
+    abstract public function find($identifier) {}
+    
+    abstract public function update($2dArray) {}
+    
+    abstract public function delete($identifier) {}
+    
+    abstract public function create($2dArray) {}
+
+
+}
