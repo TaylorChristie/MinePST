b422f40e907fe3e179662b49cd3b59939eb1fc90
Taylor <MineSQL@users.noreply.github.com>
2016-07-08 15:43:25 -0600

    Create Model.php

---
+++ vendor/MineSQL/ModelInterface/Model.php
@@ -0,0 +1,16 @@
+<?php
+namespace MineSQL\ModelInterface;
+
+use MineSQL\DBInterface;
+
+
+// all user functions will be found here and they will all use the Interface in order to complete Querys.
+// this enables the switching of database architechure extremely easy.
+class Model extends PDOInterface
+{
+
+
+
+
+}
+
