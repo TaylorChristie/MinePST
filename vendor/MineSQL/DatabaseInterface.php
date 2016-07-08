4d85950883b9d1aa166c1373489a57a9414b03b2
Taylor <MineSQL@users.noreply.github.com>
2016-07-08 15:32:04 -0600

    converted to proper interface

--- vendor/MineSQL/DatabaseInterface.php
+++ vendor/MineSQL/DatabaseInterface.php
@@ -3,20 +3,15 @@ namespace MineSQL;
 
 
 // This is a loose interface for our database which we can hook into a MYSQL 
-// implementation, and then eventually a high level Paste interaction class
-// these interactions should have a table defined.
+// implementation, and then eventually a high level Model interaction class
+interface DatabaseInterface {
 
-abstract class DatabaseInterface {
-
-    public $connection, $table;
-
-    abstract public function find($identifier) {}
+    abstract public function sendPreparedQuery($sql, $values){}
     
-    abstract public function update($2dArray) {}
+    abstract public function sendPreparedQueryWithReturn($sql, $values){}
     
-    abstract public function delete($identifier) {}
+    abstract public function insertIntoNewRow($data2d){}
     
-    abstract public function create($2dArray) {}
-
+    abstract public function updateIntoExistingRow($id, $data){}
 
 }
