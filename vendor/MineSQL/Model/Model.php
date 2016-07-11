d27d12084490258e789920f1cc9fe7f5eab2f431
Taylor <MineSQL@users.noreply.github.com>
2016-07-11 14:10:04 -0600

    Update Model.php

--- vendor/MineSQL/Model/Model.php
+++ vendor/MineSQL/Model/Model.php
@@ -1,13 +1,25 @@
 <?php
 namespace MineSQL\Model;
 
-use MineSQL\DBInterface;
+
 
 
 // all user functions will be found here and they will all use the Interface in order to complete Querys.
 // this enables the switching of database architechure extremely easy.
-class Model extends PDOInterface
+class Model
 {
+  
+    protected $dbi;
+    
+    public function __construct()
+    {
+        $this->dbi = new ConnectionManager(); // need to somehow decide how to access a certain interface easily without removing portablility
+    }
+    
+    
+    
+    
+    
 
 
 
