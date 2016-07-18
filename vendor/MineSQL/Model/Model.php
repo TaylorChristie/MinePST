5074805940647c702c4e64d5affb4c2a982e42c4
Taylor <MineSQL@users.noreply.github.com>
2016-07-18 16:25:43 -0600

    Update Model.php

--- vendor/MineSQL/Model/Model.php
+++ vendor/MineSQL/Model/Model.php
@@ -6,14 +6,31 @@ namespace MineSQL\Model;
 
 // all user functions will be found here and they will all use the Interface in order to complete Querys.
 // this enables the switching of database architechure extremely easy.
+
+//    public function sendPreparedQuery($sql, array $values = null)
+//    public function sendPreparedQueryWithReturn($sql, array $values = null, $takeOne = 0)
+
+
 class Model
 {
   
     protected $dbi;
+    protected $table_name, $primaryKey,
     
     public function __construct()
     {
         $this->dbi = new ConnectionManager(); // need to somehow decide how to access a certain interface easily without removing portablility
+    }
+    
+    public function find($primaryKey)
+    {
+      // gets one entry from table via its primary key 
+    }
+    
+    public function where($query, $values, $limit = 10)
+    {
+      
+      
     }
     
     
